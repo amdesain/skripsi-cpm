df4faf864b6b73ec153ecc23d9984a68d9495063
Bagus Kurniawan <baguskurniawan999@gmail.com>
2022-02-05 17:07:18 +0700

    WIP

--- application/views/app.php
+++ application/views/app.php
@@ -1,7 +1,7 @@
-
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -13,94 +13,144 @@
 
 
     <!-- Bootstrap core CSS -->
-<link href="<?= base_url('assets/bootstrap.min.css') ?>" rel="stylesheet" crossorigin="anonymous">
+    <link href="<?= base_url('assets/bootstrap.min.css') ?>" rel="stylesheet" crossorigin="anonymous">
 
     <!-- Favicons -->
-<meta name="theme-color" content="#7952b3">
+    <meta name="theme-color" content="#7952b3">
 
 
     <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
         }
-      }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
     </style>
 
-    
-  </head>
-  <body>
-    
-<header>
-  <div class="collapse bg-dark" id="navbarHeader">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-8 col-md-7 py-4">
-          <h4 class="text-white">About</h4>
-          <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+
+</head>
+
+<body>
+
+    <header>
+        <div class="navbar navbar-dark bg-dark shadow-sm">
+            <div class="container">
+                <a href="#" class="navbar-brand d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24">
+                        <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+                        <circle cx="12" cy="13" r="4" />
+                    </svg>
+                    <strong>Critical Path Method</strong>
+                </a>
+            </div>
         </div>
-        <div class="col-sm-4 offset-md-1 py-4">
-          <h4 class="text-white">Contact</h4>
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-white">Follow on Twitter</a></li>
-            <li><a href="#" class="text-white">Like on Facebook</a></li>
-            <li><a href="#" class="text-white">Email me</a></li>
-          </ul>
+    </header>
+
+    <main>
+
+        <section class="py-5 text-center container">
+            <div class="row py-lg-5">
+                <div class="col-lg-6 col-md-8 mx-auto">
+                    <h1 class="fw-light">Critical Path Method</h1>
+                    <p class="lead text-muted">Critical Path Method Adalah ... </p>
+                </div>
+            </div>
+        </section>
+        <section class="py-5 container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3>Kegiatan</h3>
+                    <form action="">
+                        <table class="table table-borderless table-form">
+                            <thead>
+                                <tr>
+                                    <th>Kode</th>
+                                    <th>Deskripsi</th>
+                                    <th>Bergantung</th>
+                                    <th>
+                                        <button class="btn btn-sm btn-success tambah" type="button">Tambah</button>
+                                    </th>
+                                </tr>
+                            </thead> 
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <input type="text" class="form-control" name="kode[]">
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" name="deskripsi[]">
+                                    </td>
+                                    <td>
+                                        <input type="text" class="form-control" name="bergantung[]" placeholder="eg:A,B,C">
+                                    </td>
+                                    <td></td>
+                                </tr>
+                            </tbody> 
+                            <tbody class="append"></div> 
+                        </table>
+                    </form>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <div id="blueprint"> 
+    </div>
+
+    <footer class="text-muted py-5">
+        <div class="container">
+            <p class="float-end mb-1">
+                <a href="#">Back to top</a>
+            </p>
+            <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+            <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="/docs/5.1/getting-started/introduction/">getting started guide</a>.</p>
         </div>
-      </div>
-    </div>
-  </div>
-  <div class="navbar navbar-dark bg-dark shadow-sm">
-    <div class="container">
-      <a href="#" class="navbar-brand d-flex align-items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
-        <strong>Critical Path Method</strong>
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    </div>
-  </div>
-</header>
-
-<main>
-
-  <section class="py-5 text-center container">
-    <div class="row py-lg-5">
-      <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">Album example</h1>
-        <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-        <p>
-          <a href="#" class="btn btn-primary my-2">Main call to action</a>
-          <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-        </p>
-      </div>
-    </div>
-  </section>
-
-</main>
-
-<footer class="text-muted py-5">
-  <div class="container">
-    <p class="float-end mb-1">
-      <a href="#">Back to top</a>
-    </p>
-    <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-    <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="/docs/5.1/getting-started/introduction/">getting started guide</a>.</p>
-  </div>
-</footer>
+    </footer>
 
 
-    <script src="<?= base_url('assets/bootstrap.bundle.min.js') ?>/docs/5.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="<?= base_url('assets/bootstrap.bundle.min.js') ?>" crossorigin="anonymous"></script>
+    <script src="<?= base_url('assets/jquery.min.js') ?>" crossorigin="anonymous"></script>
 
-      
-  </body>
+    <script>
+        let row = `<tr>
+            <td>
+                <input type="text" class="form-control" name="kode[]">
+            </td>
+            <td>
+                <input type="text" class="form-control" name="deskripsi[]">
+            </td>
+            <td>
+                <input type="text" class="form-control" name="bergantung[]" placeholder="eg:A,B,C">
+            </td>
+            <td>
+                <button class="btn btn-sm btn-danger hapus" type="button">Hapus</button>
+            </td>
+        </tr>`
+
+        $(document).ready(function () {
+            $(document).on('click', '.tambah', function () {
+                var table = $('.table-form');
+
+                table.find('tbody.append').append(row);
+            });
+
+            $(document).on('click', '.hapus', function () {
+                var current_row = $(this).parents('tr');
+                current_row.remove();
+            });
+        });
+    </script>
+
+
+</body>
+
 </html>
